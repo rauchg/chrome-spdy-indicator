8e7bf12a87f7dbdfe22a1e61e610bfe489db77e8
Devin Rhode <devinrhode2@gmail.com>
2012-03-10 13:16:19 -0800

    simplified things

--- content.js
+++ content.js
@@ -1,7 +1,2 @@
-
 // send spdy bool for current page
-if ('undefined' != typeof window && window.chrome) {
-  chrome.extension.sendRequest({ spdy: window.chrome.loadTimes().wasFetchedViaSpdy });
-} else {
-  chrome.extension.sendRequest({ spdy: null });
-}
+chrome.extension.sendRequest({ spdy: window.chrome.loadTimes().wasFetchedViaSpdy });

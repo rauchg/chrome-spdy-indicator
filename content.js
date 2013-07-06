75d2151e8cd2f4ca3965a4c54ea5354b4c3d6135
MATSUU Takuto <matsuu@gmail.com>
2013-07-06 15:40:12 +0900

    Fix comment

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function (res, sender, sendResponse) {
   var loadTimes = window.chrome.loadTimes();
-  // send spdy bool for current page
+  // send spdy info for current page
   chrome.runtime.sendMessage({
     spdy: loadTimes.wasFetchedViaSpdy,
     info: loadTimes.connectionInfo || loadTimes.npnNegotiatedProtocol

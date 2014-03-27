da8b08590ee4680453811fb493cfe28754fa0381
Matt Hickford <matt.hickford@gmail.com>
2014-03-27 21:44:36 +0000

    Omit URL from tooltip
    Long URL in tooltip made tooltip hard to read.

--- indicator.js
+++ indicator.js
@@ -46,7 +46,7 @@ chrome.runtime.onMessage.addListener(function (res, sender) {
 
     // change icon tooltip
     chrome.pageAction.setTitle({
-        title: tab.url + ' is ' + tooltip + '-enabled' + (res.spdy ? '(' + res.info + ')' : '')
+        title: tooltip + '-enabled' + (res.spdy ? '(' + res.info + ')' : '')
       , tabId: tab.id
     });
 

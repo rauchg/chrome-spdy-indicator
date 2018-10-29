e6e876c2db459a701aed02bb41a7b6474b6e7db9
Josh Soref <jsoref@users.noreply.github.com>
2018-10-29 12:08:02 -0400

    Refactor sendInfo

--- content.js
+++ content.js
@@ -1,13 +1,15 @@
 var loadTimes = window.chrome.loadTimes();
-// send spdy info for current page
-chrome.runtime.sendMessage({
-  spdy: loadTimes.wasFetchedViaSpdy,
-  info: loadTimes.npnNegotiatedProtocol || loadTimes.connectionInfo
-});
 
-chrome.runtime.onMessage.addListener(function (res, sender, sendResponse) {
+function sendInfo() {
   chrome.runtime.sendMessage({
     spdy: loadTimes.wasFetchedViaSpdy,
     info: loadTimes.npnNegotiatedProtocol || loadTimes.connectionInfo
   });
+}
+
+// send spdy info for current page
+sendInfo();
+
+chrome.runtime.onMessage.addListener(function (res, sender, sendResponse) {
+  sendInfo();
 });
